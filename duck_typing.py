8160d2d18b08069c253fe05ec45b4604a80293cc
CUDA <harisenkumar1997@gmail.com>
2021-05-25 18:46:45 +0530

    lec 22 left and lec 23 duck typing comleted

---
+++ duck_typing.py
@@ -0,0 +1,17 @@
+#Duck Typing
+class Duck:
+    def walk(self):
+        print("thapak thapak thapak thapak")
+class Horse:
+    def walk(self):
+        print("tabdak tabdak tabdak tabdak")
+
+class Cat:
+    def talk(self):
+        print("Meow Meow")
+
+def myfunction(obj):
+    obj.talk()
+
+c= Cat()
+myfunction(c)
